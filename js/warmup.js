18541ab4319d8df736a3affd7497243731320640
Christian Parker <christianparker512@gmail.com>
2020-09-28 10:05:28 -0500

    Javascript with HTML changes

--- js/warmup.js
+++ js/warmup.js
@@ -1,7 +1,15 @@
 "use strict";
-console.log("Vivian");
-console.log("Sophie");
-console.log("Matt");
-console.log("Kaitlin");
-console.log("Connor");
+// console.log("Vivian");
+// console.log("Sophie");
+// console.log("Matt");
+// console.log("Kaitlin");
+// console.log("Connor");
 
+var name3 = "Correy";
+function sayHello(name){
+    console.log("Hello " + name);
+}
+sayHello(name: "Vivian");
+sayHello(name: "Sophie");
+sayHello(name: "Matt");
+sayHello(name: "Kaitlin");

83ebe6ca0cb416f588d85e6f16b51b44131a8d15
Christian Parker <christianparker512@gmail.com>
2020-10-02 16:58:57 -0500

    For Loops and Break and Continue

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,10 +1,12 @@
-
-var response = prompt("Please enter an odd number between 1 and 50");
+// Create a file named break_and_continue.js in the js directory.
+//     Prompt the user for an odd number between 1 and 50. Use a loop and a break statement to continue prompting the user if they enter invalid input.
+//     Use a loop and the continue statement to output all the odd numbers between 1 and 50, except for the number the user entered.
+// var response = prompt("Please enter an odd number between 1 and 50");
 for (var i = 1; i <= 50; i++) {
     if (i % 2 === 0) {
         continue;
     } else if (response == i) {
-        console.log("Yikes!! Number to skip is: " + i);
+        console.log("Yikes!! Skipping number: " + i);
     } else {
         console.log("Here is an odd number: " + i);
     }

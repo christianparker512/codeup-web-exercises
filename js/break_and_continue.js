445784dba25be8333555c458829228c6f1eefead
Christian Parker <christianparker512@gmail.com>
2020-10-05 12:13:13 -0500

    break and continue with solutions

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -14,3 +14,29 @@ for (var i = 1; i <= 50; i++) {
         break;
     }
 }
+
+do {
+    var userNumber = Number(prompt("Pick an odd number between 1 and 50"));
+   if(userNumber < 1 || userNumber > 50){
+       alert("Number is invalid. Please try again.");
+   } else if (if userNumber % 2 === 0) {
+       alert(userNumber + " is not odd, please try again.");
+
+   } else if(isNana(userNumber)) {
+           alert(userNumber + " is not a number, please try again."
+   } else {
+       alert("Number is valid! Thank you!";
+       break;
+   }
+} while(true);
+
+for (var i =1; i <= 50; i ++){
+    if (i % 2 === 0) {
+        continue;
+    }
+    if(i === userNumber){
+        console.log("Yikes! Skipping number: " +1);
+    } else {
+        console.log("Here is an odd number: " +1);
+    }
+}

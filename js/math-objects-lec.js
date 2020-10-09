0b6bcfd2ea6317592e5bf0a8aba681f6d39c9803
Christian Parker <christianparker512@gmail.com>
2020-10-09 16:10:41 -0500

    Math Objects Lecture - on the plane

---
+++ js/math-objects-lec.js
@@ -0,0 +1,18 @@
+let inputString = "Trick or Treat";
+let inputArray = inputString.split(' ');
+inputArray.forEach(function(element) {
+    console.log(element);
+});
+
+math.random()
+var mathComputations = new Math();
+// don't do this.'
+
+let randomNum = Math.random() *2;
+    console.log(randomNum);
+
+// Math.Round()
+let index= Math.round(randomNum);
+console.log(index);
+// we can't use a decimal when referring to an array index, so let's round it to the nearest integer
+Math.floor()

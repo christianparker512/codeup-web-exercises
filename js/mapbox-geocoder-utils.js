dd02845772a9cbc5cb6b1382346f6f8707e91d29
Christian Parker <christianparker512@gmail.com>
2020-11-10 11:39:38 -0600

    mapbox and jquery

--- js/mapbox-geocoder-utils.js
+++ js/mapbox-geocoder-utils.js
@@ -13,15 +13,17 @@
  *  })
  *
  */
-function geocode(search, token) {
+function geocode(search"46 westerleigh, san antonio, texas", mapboxToken) {
     var baseUrl = 'https://api.mapbox.com';
     var endPoint = '/geocoding/v5/mapbox.places/';
-    return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
+    return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + mapboxToken)
         .then(function(res) {
             return res.json();
             // to get all the data from the request, comment out the following three lines...
         }).then(function(data) {
             return data.features[0].center;
+
+            map.addControl(new mapboxgl.NavigationControl());
         });
 }
 
@@ -39,15 +41,96 @@ function geocode(search, token) {
  *  })
  *
  */
-function reverseGeocode(coordinates, token) {
-    var baseUrl = 'https://api.mapbox.com';
-    var endPoint = '/geocoding/v5/mapbox.places/';
-    return fetch(baseUrl + endPoint + coordinates.lng + "," + coordinates.lat + '.json' + "?" + 'access_token=' + token)
-        .then(function(res) {
-            return res.json();
-        })
-        // to get all the data from the request, comment out the following three lines...
-        .then(function(data) {
-            return data.features[0].place_name;
-        });
-}
+
+// function reverseGeocode(coordinates, token) {
+//     var baseUrl = 'https://api.mapbox.com';
+//     var endPoint = '/geocoding/v5/mapbox.places/';
+//     return fetch(baseUrl + endPoint + coordinates.lng + "," + coordinates.lat + '.json' + "?" + 'access_token=' + token)
+//         .then(function(res) {
+//             return res.json();
+//         })
+//         // to get all the data from the request, comment out the following three lines...
+//         .then(function(data) {
+//             return data.features[0].place_name;
+//         });
+// }
+// function reverseGeocode(coordinates, token) {
+//     var baseUrl = 'https://api.mapbox.com';
+//     var endPoint = '/geocoding/v5/mapbox.places/';
+//     return fetch(baseUrl + endPoint + coordinates.lng + "," + coordinates.lat + '.json' + "?" + 'access_token=' + token)
+//         .then(function(res) {
+//             return res.json();
+//         })
+//         // to get all the data from the request, comment out the following three lines...
+//         .then(function(data) {
+//             return data.features[0].place_name;
+//         });
+// }
+
+// function geocode(search, token) {
+//     var baseUrl = 'https://api.mapbox.com';
+//     var endPoint = '/geocoding/v5/mapbox.places/';
+//     return fetch(baseUrl + endPoint + encodeURIComponent(search) + '.json' + "?" + 'access_token=' + token)
+//         .then(function (res) {
+//             return res.json();
+//             // to get all the data from the request, comment out the following three lines...
+//         }).then(function (data) {
+//             return data.features[0].center;
+//         });
+// }
+
+geocode("3703 19th Street, Lubbock, TX", mapboxToken)
+    .then(function (result) {
+        console.log('Jazz...A Louisiana Kitchen! ' + result);
+        map.setCenter(result);
+        map.setZoom(15);
+
+        new mapboxgl.Marker()
+        .setLngLat(result)
+        .addTo(map);
+    });
+
+geocode("401 S Alamo St, San Antonio, TX, 78205", mapboxToken)
+    .then(function (result) {
+        console.log('Love me some Pasta at Nonna Osteria! ' + result);
+        map.setCenter(result);
+        map.setZoom(15);
+
+        new mapboxgl.Marker()
+            .setLngLat(result)
+            .addTo(map);
+    });
+
+geocode("3714 Broadway, San Antonio, TX", mapboxToken)
+    .then(function (result) {
+        console.log('Best BBQ in town! ' + result);
+        map.setCenter(result);
+        map.setZoom(15);
+
+        new mapboxgl.Marker()
+            .setPopUp
+            .setLngLat(result)
+            .addTo(map);
+    });
+
+//Reverse Geocode
+// function reverseGeocode(coordinates, token) {
+//     var baseUrl = 'https://api.mapbox.com';
+//     var endPoint = '/geocoding/v5/mapbox.places/';
+//     return fetch(baseUrl + endPoint + coordinates.lng + "," + coordinates.lat + '.json' + "?" + 'access_token=' + token)
+//         .then(response => response.json())
+//         .then(data => {
+//             return data
+//         })
+// };
+
+// var clementine ={lng: -98.5219, lat: 29.5330}
+//
+// reverseGeocode(clementine, mapboxToken)
+//     .then(function (result) {
+//         console.log("Clementine and their tasting menu! " + result)
+//
+//         new mapboxgl.Marker()
+//             .setLngLat(result)
+//             .addTo(map);
+//     })
--- js/mapbox.js
+++ js/mapbox.js
@@ -1,3 +1,4 @@
+"use strict";
 /**********************************************
  * 			SETTING UP KEYS.JS
  *********************************************/
@@ -12,20 +13,23 @@
 
 //TODO TOGETHER: Set map to san antonio area using the coordinates [-98.4916, 29.4252]
 
-    mapboxgl.accessToken = mapboxToken;
+mapboxgl.accessToken = mapboxToken;
 
-    var mapOptions = {
-    // var map = new mapboxgl.Map({
+
+var mapOptions = {
     container: 'map',
-    style: 'mapbox://styles/mapbox/dark-v10', // stylesheet location
-    center: [-97.8898, 30.1911], // starting position [lng, lat]
-    // center: [-98.4916, 29.4252], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+    center: [-98.4916, 29.4252], // starting position [lng, lat]
+    zoom: 14 // starting zoom
+}
 
-    // End of Mapbox Script
-    }
 var map = new mapboxgl.Map(mapOptions);
+
+map.addControl(new mapboxgl.NavigationControl());
+
 //TODO: Experiment with different map styles, zoom levels, and centers. You will need to reference the mapbox docs. (~10 minutes)
+
+
 
 
 /**********************************************
@@ -37,15 +41,24 @@ var map = new mapboxgl.Map(mapOptions);
 
 
 // TODO TOGETHER: Add a marker to the map using the following coordinates [-98.4916, 29.4260]. This marker will mark the Alamo on our map.
+
+var markerOptions = {
+    color: "#ff0000",
+    draggable: true
+}
+
+
+var alamoMarker = new mapboxgl.Marker(markerOptions)
+    .setLngLat([-98.4861, 29.4260])
+    .addTo(map);
+
+
+
 // TODO TOGETHER: Change the color of the marker
 
-    var markerOptions = {
-        color: "#ff0000",
-        draggable: true
-    }
-    var alamoMarker = new mapboxgl.Marker(markerOptions)
-        .setLngLat([-98.4861, 29.4260])
-        .addTo(map);
+
+
+
 
 // TODO: Experiment with the color, and setting the LngLat
 // TODO: Update the marker object to make the marker draggable. *Hint: reference the docs!
@@ -59,11 +72,23 @@ var map = new mapboxgl.Map(mapOptions);
 
 
 // TODO TOGETHER: Add a popup to the map over codeup. Set the html as a paragraph that says "Codeup Rocks!"
+
+// var popup = new mapboxgl.Popup()
+// 	.setLngLat([-98.489615, 29.426827])
+// 	.setHTML("<h6>Codeup Rocks</h6>")
+// 	.addTo(map);
+
+
+
 // TODO TOGETHER: Comment out the popup we just added. Add a popup to the alamo marker.
-var popup = new mapboxgl.Popup()
-    .setLngLat([-98.489615, 29.426827])
-    .setHTML("<h1>Codeup Rocks</h1>")
+
+var alamoPopup = new mapboxgl.Popup()
+    .setHTML("<p>Remember the Alamo</p>")
     .addTo(map);
+
+alamoMarker.setPopup(alamoPopup);
+
+
 
 // TODO: Review the popup docs. What are some additional options we can pass to the popup?
 // TODO: Try setting the text by using ".setText()" instead of ".setHTML()"
@@ -74,22 +99,52 @@ var popup = new mapboxgl.Popup()
  * 					Geocoder
  *********************************************/
 // Geocoding Docs --> https://docs.mapbox.com/api/search/#geocoding
-geocode("600 Navarro St #350, San Antonio, TX 78205", mapboxToken).then(function(result) {
-    console.log(result);
-    map.setCenter(result);
-    map.setZoom(20);
-});
+
+// the  geocode method from mapbox-geocoder-utils.js
+
 
 // TODO TOGETHER: Using the Geocoder helper function, log the coordinates of Codeup and recenter the map to focus on Codeup.
 //https://docs.mapbox.com/mapbox-gl-js/api/map/#map#setcenter
 
+// geocode("8300 East Lawther Drive, Dallas, TX", mapboxToken).then(function (result) {
+// 	console.log(result);
+// 	map.setCenter(result);
+// 	map.setZoom(20);
+//
+// })
+
 
 //TODO: Using the geocode method above, add a marker at Codeup to the map
+
+geocode("600 Navarro St #350, San Antonio, TX 78205 ", mapboxToken).then(function (result) {
+    console.log(result);
+    map.setCenter(result);
+    map.setZoom(15);
+
+    new mapboxgl.Marker()
+        .setLngLat(result)
+        .addTo(map);
+
+})
+
+
+
 //TODO: Instead of setCenter try using map.jumpTo()
 //TODO: Instead of setCenter try using map.flyTo()
 
 
 
 // TODO TOGETHER: Reverse Geocoding: Using the reverse geocoding method, enter the coordinates {lng: -98.4861, lat: 29.4260} to get a physical address for the alamo
+
+
+reverseGeocode({lng: -98.4861, lat: 29.4260}, mapboxToken).then(function (result) {
+    console.log(result);
+})
+
+
+
+
+
+
 // TODO: Reverse geocode coordinates of your choice using the reverse geocode method
 

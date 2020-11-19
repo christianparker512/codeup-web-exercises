9a537413bcc43ff10ac8f97167b05fbbafaefc4c
Christian Parker <christianparker512@gmail.com>
2020-11-19 14:31:55 -0600

    es6 lecture initial commit

---
+++ js/es6-lec-pt2-objects.js
@@ -0,0 +1,30 @@
+// * **************************************************************
+// *
+// *       Object Property Variable Assignment Shorthand
+// *
+// *****************************************************************/
+//TODO TOGETHER: Create a newDog object using shorthand syntax
+
+// let breed = "Pug";
+// let age = 3;
+// let dogName = "Lexie";
+// let isCute = true;
+//
+// //old way
+// let oldDog = {
+// 	breed: breed,
+// 	age: age,
+// 	dogName: dogName,
+// 	isCute: isCute
+// }
+//
+// console.log(oldDog);
+//
+
+
+
+
+
+// //new way
+
+
